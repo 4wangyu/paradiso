374bb01eae10da984932e7b18a003ef1191b9829
4wangyu <4wangyu@gmail.com>
2020-07-15 21:30:42 +0800

    finish scan

--- cmd/scan.go
+++ cmd/scan.go
@@ -17,15 +17,15 @@ var scanCmd = &cobra.Command{
 	Short: "Scans all media under specified path.",
 	Long:  `Scans all media under specified path.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		initDB()
+		dbFile := initDB()
 
 		if len(args) > 0 {
 			if filepath.IsAbs(args[0]) {
-				iterate(args[0])
+				scan(args[0], dbFile)
 			} else {
 				wd, err := os.Getwd()
 				check(err)
-				iterate(fmt.Sprintf("%s/%s", wd, args[0]))
+				scan(fmt.Sprintf("%s/%s", wd, args[0]), dbFile)
 			}
 		}
 	},
@@ -35,30 +35,30 @@ func init() {
 	rootCmd.AddCommand(scanCmd)
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func initDB() {
+func initDB() string {
 	f, err := os.Create(fmt.Sprintf("media.%s.db", time.Now().Format("20060102150405")))
 	check(err)
 
 	db, err := sql.Open("sqlite3", f.Name())
 	check(err)
 
-	_, err = db.Exec(`create table media (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, path TEXT NOT NULL);`)
+	_, err = db.Exec(`create table media (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, path TEXT NOT NULL, date INTEGER);`)
 	check(err)
 
 	db.Close()
+
+	return f.Name()
 }
 
-func iterate(path string) {
+func scan(path string, dbFile string) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		fmt.Printf("Path %s does not exist.\n", path)
 		os.Exit(1)
 	}
+
+	db, err := sql.Open("sqlite3", dbFile)
+	check(err)
+	defer db.Close()
 
 	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		check(err)
@@ -69,7 +69,11 @@ func iterate(path string) {
 
 		extension := filepath.Ext(path)
 		if contains([]string{".mp4", ".mkv"}, extension) {
-			fmt.Printf("Path: %s\nFile Name: %s\n", path, info.Name())
+			stmt, err := db.Prepare("INSERT INTO media(path, name, date) values(?,?,?)")
+			check(err)
+
+			_, err = stmt.Exec(path, info.Name(), info.ModTime().Unix())
+			check(err)
 		}
 
 		return nil
@@ -83,4 +87,10 @@ func contains(slice []string, val string) bool {
 		}
 	}
 	return false
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
