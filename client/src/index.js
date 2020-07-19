acdcad72517b29364c812caa6c553a57be45172a
4wangyu <4wangyu@gmail.com>
2020-07-19 22:57:00 +0800

    remove styles and reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './store/reducers';
-import promise from 'redux-promise';
 import '@babel/polyfill';
 
 import App from './containers/App';
 import './static/sass/style.scss';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
-
-const app = (
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-);
+const app = <App />;
 
 ReactDOM.render(app, document.getElementById('app'));
