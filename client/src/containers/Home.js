0c527e9f7c35e53235d21a651ff386ae13003e6d
4wangyu <4wangyu@gmail.com>
2020-07-19 22:38:50 +0800

    remove modal

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import axios from '../axios-movies';
-
-import Navbar from './Navbar';
 import MainContent from './MainContent';
-import Movie from '../components/Movie/Movie';
-import Modal from '../components/UI/Modal';
-import MovieDetails from '../components/Movie/MovieDetails';
 
 class Home extends Component {
   state = {
@@ -14,30 +8,6 @@ class Home extends Component {
     /** Holds the movie information for a single movie. */
     movieOverview: {},
   };
-
-  /* Get the appropriate details for a specific movie that was clicked */
-  // selectMovieHandler = (movie) => {
-  //   this.setState({ toggleModal: true });
-
-  //   let url;
-  //   /** Make the appropriate API call to get the details for a single movie or tv show. */
-  //   if (movie.media_type === "movie") {
-  //     const movieId = movie.id;
-  //     url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`;
-
-  //   } else if (movie.media_type === "tv") {
-  //     const tvId = movie.id
-  //     url = `https://api.themoviedb.org/3/tv/${tvId}?api_key=${process.env.API_KEY}`;
-  //   }
-
-  //   axios.get(url)
-  //     .then(res => {
-  //       const movieData = res.data;
-  //       this.setState({ movieOverview: movieData });
-  //     }).catch(error => {
-  //       console.log(error);
-  //     });
-  // }
 
   closeModal = () => {
     this.setState({ toggleModal: false });
@@ -49,13 +19,6 @@ class Home extends Component {
         <div className="main-content">
           <MainContent />
         </div>
-        <Modal
-          show={this.state.toggleModal}
-          modalClosed={this.closeModal}
-          movie={this.state.movieOverview}
-        >
-          <MovieDetails movie={this.state.movieOverview} />
-        </Modal>
       </>
     );
   }
