8ebaf8d586e2845cf09d8e451b77ac2901d582d1
4wangyu <4wangyu@gmail.com>
2020-07-19 17:24:49 +0800

    add client

---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const footer = () => (
+  <footer className="footer">
+    <div className="footer__copyright">&copy; 2020 Made with ❤️</div>
+  </footer>
+);
+
+export default footer;
---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './store/reducers';
+import promise from 'redux-promise';
+import '@babel/polyfill';
+
+import App from './containers/App';
+import './static/sass/style.scss';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+const app = (
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('app'));
