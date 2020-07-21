a032a5ed380d1a9fb542ffbbc61a8b9fb618f004
4wangyu <4wangyu@gmail.com>
2020-07-22 00:45:51 +0800

    skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Videos from "./pages/Videos";
+import Search from "./pages/Search";
+import Home from "./pages/Home";
+import Recent from "./pages/Recent";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Switch>
+        <Route path="/videos">
+          <Videos />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/recent">
+          <Recent />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
