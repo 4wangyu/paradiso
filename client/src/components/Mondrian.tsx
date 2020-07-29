a226698179ac95dc7c039140222204611fc1da7f
4wangyu <4wangyu@gmail.com>
2020-07-30 01:05:22 +0800

    add mondrian component

---
+++ client/src/components/Mondrian.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useRef } from "react";
+
+interface Square {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color?: string;
+}
+
+const squares: Square[] = [];
+
+const colors = ["#D40920", "#1356A2", "#F7D842"];
+const white = "#fff";
+
+function splitSquaresWith(coordinates: Square) {
+  const { x, y } = coordinates;
+
+  for (let i = squares.length - 1; i >= 0; i--) {
+    const square = squares[i];
+
+    if (x && x > square.x && x < square.x + square.width) {
+      if (Math.random() > 0.5) {
+        squares.splice(i, 1);
+        splitOnX(square, x);
+      }
+    }
+
+    if (y && y > square.y && y < square.y + square.height) {
+      if (Math.random() > 0.5) {
+        squares.splice(i, 1);
+        splitOnY(square, y);
+      }
+    }
+  }
+}
+
+function splitOnX(square: Square, splitAt: number) {
+  const squareA = {
+    x: square.x,
+    y: square.y,
+    width: square.width - (square.width - splitAt + square.x),
+    height: square.height,
+  };
+
+  const squareB = {
+    x: splitAt,
+    y: square.y,
+    width: square.width - splitAt + square.x,
+    height: square.height,
+  };
+
+  squares.push(squareA);
+  squares.push(squareB);
+}
+
+function splitOnY(square: Square, splitAt: number) {
+  const squareA = {
+    x: square.x,
+    y: square.y,
+    width: square.width,
+    height: square.height - (square.height - splitAt + square.y),
+  };
+
+  const squareB = {
+    x: square.x,
+    y: splitAt,
+    width: square.width,
+    height: square.height - splitAt + square.y,
+  };
+
+  squares.push(squareA);
+  squares.push(squareB);
+}
+
+function draw(context: CanvasRenderingContext2D) {
+  for (let i = 0; i < colors.length; i++) {
+    squares[Math.floor(Math.random() * squares.length)].color = colors[i];
+  }
+  for (let i = 0; i < squares.length; i++) {
+    context.beginPath();
+    context.rect(
+      squares[i].x,
+      squares[i].y,
+      squares[i].width,
+      squares[i].height
+    );
+
+    const color = squares[i].color || white;
+    context.fillStyle = color;
+
+    context.fill();
+    context.stroke();
+  }
+}
+
+interface PropType {
+  width: number;
+  height: number;
+}
+
+const Mondrian = ({ width, height }: PropType) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas?.getContext("2d");
+
+    if (canvas && context) {
+      canvas.width = width;
+      canvas.height = height;
+      context.lineWidth = 5;
+
+      const size = Math.max(width, height);
+      const step = size / 7;
+      squares.push({
+        x: 0,
+        y: 0,
+        width,
+        height,
+      });
+      for (let i = 0; i < size; i += step) {
+        splitSquaresWith({ y: i } as Square);
+        splitSquaresWith({ x: i } as Square);
+      }
+
+      draw(context);
+    }
+  });
+
+  return <canvas ref={canvasRef} />;
+};
+
+export default Mondrian;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Mondrian from "../components/Mondrian";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Mondrian width={window.innerWidth} height={window.innerHeight} />
+    </div>
+  );
 };
 
 export default Home;
