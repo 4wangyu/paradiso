3e1ed3ffe03fe2c76fc61006a34723a9a5717c04
4wangyu <4wangyu@gmail.com>
2020-08-02 23:17:25 +0800

    search page

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Grid from "../components/Grid";
+import Pagination from "../components/Pagination";
 
 const Search = () => {
-  return <div></div>;
+  return (
+    <div className="search">
+      <h2>
+        Search results for
+        <input autoFocus></input>
+      </h2>
+      <Grid></Grid>
+      <Pagination></Pagination>
+    </div>
+  );
 };
 
 export default Search;
