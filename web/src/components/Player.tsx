5c842966ba9f1f12a73f9c35463d3d0761f128a9
4wangyu <4wangyu@gmail.com>
2020-08-10 13:11:58 +0800

    player page

--- web/src/components/Player.tsx
+++ web/src/components/Player.tsx
@@ -1,21 +1,40 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Plyr from "plyr";
 import "plyr/dist/plyr.css";
+import { ReactComponent as LeftIcon } from "../icons/left.svg";
+import classNames from "classnames";
+
+const id = "dddef4f7-d9cc-4b0e-8aba-149c3d4f4f6f";
 
 const Player = () => {
+  const [controlsshown, setControlsshown] = useState<boolean>(false);
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
     const player = new Plyr(videoRef.current as HTMLVideoElement, {
       autoplay: true,
     });
+    player.on("controlsshown", () => setControlsshown(true));
+    player.on("controlshidden", () => setControlsshown(false));
     return () => player.destroy();
+  }, []);
+
+  const headerClass = classNames({
+    header: true,
+    controlsshown,
   });
 
   return (
-    <div>
+    <div className="player">
+      <div className={headerClass}>
+        <button>
+          <LeftIcon />
+        </button>
+        <h1>Title</h1>
+        <button></button>
+      </div>
       <video ref={videoRef}>
-        <source src="http://localhost:58135/random" />
+        <source src={`http://localhost:9000/file?id=${id}`} />
       </video>
     </div>
   );
