11de476c1c8d5eb73e88421561c5a9d858439e33
4wangyu <4wangyu@gmail.com>
2020-08-10 14:49:10 +0800

    add progress bar

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/style.scss";
 import App from "./App";
+import { loadProgressBar } from "axios-progress-bar";
+import "axios-progress-bar/dist/nprogress.css";
+
+loadProgressBar();
 
 ReactDOM.render(
   <React.StrictMode>
--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Recent from "../components/Recent";
 import Video from "../components/Video";
+import axios from "axios";
+
+const call = () => {
+  axios
+    .get("/random")
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+};
 
 const Home = () => {
+  useEffect(() => {
+    call();
+  }, []);
+
   return (
     <div>
       <Video
@@ -12,6 +30,7 @@ const Home = () => {
         hideTitle={true}
       />
       <Recent />
+      <button onClick={call}></button>
     </div>
   );
 };
